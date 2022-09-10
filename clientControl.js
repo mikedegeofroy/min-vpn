3b8e75a3e26319925158dadf8704057d40a9a60e
Mike de Geofroy <59496187+MikeDeGeofroy@users.noreply.github.com>
2022-09-11 01:11:24 +0300

    this is very wrong

--- clientControl.js
+++ clientControl.js
@@ -7,11 +7,13 @@ import mongoose from 'mongoose';
 let dbURI = 'mongodb://127.0.0.1:27017/min-vpn'
 
 var PaymentSchema = new mongoose.Schema({
-    uuid: { type: String, unique: true, default: function genUUID() {
-        return uuid.v4()
-    }},
-    ammount: {type: Number, required: true},
-    date: {type: Date, required: true}
+    uuid: {
+        type: String, unique: true, default: function genUUID() {
+            return uuid.v4()
+        }
+    },
+    ammount: { type: Number, required: true },
+    date: { type: Date, required: true }
 }, { collection: "payments" });
 
 mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, error => {
@@ -24,77 +26,57 @@ mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, err
 
 import express from 'express';
 var app = express();
-app.use(express.json()) 
+app.use(express.json())
 
 // The idea is to have a recurring check every 12h for the membership, if it is spoiled, then send a message asking if the person wants to renew their subscription. 
 
-app.listen(3001, function(err) {
-    if(err){
-       console.log(err);
-       } else {
-       console.log("Listening on port 3001");
+app.listen(3001, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening on port 3001");
     }
 });
 
 app.post('/callback', async (req, res) => {
-	console.log(req.body)
+    console.log(req.body)
 
-	// let uuid = req.query.uuid
+    let payments = mongoose.model("payments", PaymentSchema);
 
-    // let url = 'https://api.yookassa.ru/v3/payments';
+    if (req.body.event == 'payment.succeeded') {
+        let uuid = req.body.object.id
 
-    // let options = {
-    //     method: 'POST',
-    //     headers: {
-    //         'Idempotence-Key': uuid,
-    //         'Content-Type': 'application/json',
-    //         Authorization: 'Basic OTM1NzMyOnRlc3RfaTVqSUF0RHlFUGZHSGw5YWVhYmlwb3VyUW5FUkJ1ZndQN2RVd29SdTljdw=='
-    //     }
-    // };
+        await payments.findOneAndUpdate({ "uuid": uuid }, { status: "succeeded" }, (err, result) => {
+            if (err) {
+                // console.log(err);
+            } else {
+                // console.log(result);
+            }
+        }).clone()
 
-    // await fetch(url, options)
-    //     .then(res => res.json())
-    //     .then((json) => {
-    //         console.log(json)
-    //     })
-    //     .catch(err => console.error('error:' + err));
+        let url = 'http://localhost:3000/update';
 
-	// let payments = mongoose.model("payments", PaymentSchema);
+        let options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: `{"key":${req.body.object.metadata.key}, "type":"added"}`
+        };
+    
+        fetch(url, options)
+            .then(res => res.json())
+            .then(json => console.log(json))
+            .catch(err => console.error('error:' + err));
 
-	// await payments.findOneAndUpdate({ "uuid": uuid }, { status: "completed" }, (err, result) => {
-    //     if (err) {
-    //         // console.log(err);
-    //     } else {
-    //         // console.log(result);
-    //     }
-    // }).clone()
+    }
 
-
-	// let url2 = 'http://localhost:3000/update';
-
-	// Probably interact with mongodb 
-
-	// let options2 = {
-	// 	method: 'POST',
-	// 	headers: {'Content-Type': 'application/json'},
-	// 	body: `{"key":${key}, "type":"added"}`
-	// };
-
-	// fetch(url2, options2)
-	// 	.then(res => res.json())
-	// 	.then(json => console.log(json))
-	// 	.catch(err => console.error('error:' + err));
-
-	
-	// res.redirect('https://t.me/minvpnbot')
-    res.sendStatus(status)
+    res.sendStatus(200)
 })
 
 process.on('uncaughtException', function (err) {
     console.log(err);
-}); 
+});
 
 
-function checkPayment(){
-	return true
+function checkPayment() {
+    return true
 }
